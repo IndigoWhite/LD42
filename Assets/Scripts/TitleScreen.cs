09f5c80de0e868718b97ded94b810a52e21c7121
Sam Welsh <indiplays@gmail.com>
2018-08-11 08:26:54 +0100

    First staging/commit

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+    public GameObject[] uiPanels;
+    private int currentPanelSelected = 0;
+
+	public void AdvancePanel()
+    {
+        uiPanels[currentPanelSelected].SetActive(false);
+        currentPanelSelected += 1;
+        uiPanels[currentPanelSelected].SetActive(true);        
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        if (currentPanelSelected+1 == uiPanels.Length)
+        {
+            SceneManager.LoadScene("Main");
+            return;
+        }      
+    }
+
+}
